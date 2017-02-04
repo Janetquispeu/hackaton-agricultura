0b9a7453822dd724bb8383be9077eff583ab50a2
smiling girl.. sonrie   siempre  en la   vida <smiling girl.. sonrie   siempre  en la   vida>
2017-02-04 06:41:15 -0500

    insertando buscador

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,7 @@
+var template="<div class='contenedor-resultados'><div class='col-md-6'><div>{{latiude}}</div><div>{{longitude}}</div><div>{{nombre}}</div></div><div class='col-md-6'><img src='{{url}}' class='img-url'></div></div>"
+var contenedor=$("#contenedor");
+var map;
+
 function hideLoader(){
   document.getElementById('floatingBarsG').style.display="none";
 }
@@ -18,3 +22,63 @@ function hideLoader(){
     });  
   });
 })();
+
+
+function initMap(){
+  var mapProp = {
+      center: new google.maps.LatLng(-12.117917 ,-77.037708),
+      zoom: 6,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map'), mapProp);
+
+  function leeJson(){
+    var html=" ";
+    $.getJSON("https://janetquispeu.github.io/practicando/lugares.json",function(datos){
+
+      var val1=$("#palta").val();
+      var val2=$("#esparragos").val();
+
+      $("#btn-buscar").click(function(){
+        if(val1=="1"){
+
+          $.each(datos["palta"], function(idx,primo) {
+
+            html+="<br>"+ template.replace("{{latiude}}","latitud:"+primo.latitud)
+                          .replace("{{longitude}}","longitud:"+primo.longitud)
+                          .replace("{{nombre}}","Nombre:"+primo.nombre)
+                          .replace("{{url}}",primo.url);
+                          console.log(primo.url);
+                          console.log(primo.nombre);
+
+            var lat=parseFloat(primo.latitud);
+            var lon=parseFloat(primo.longitud);
+            console.log(lat);
+            console.log(lon);
+
+            function createMarker(lat,lon) {
+              var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(lat, lon),
+                // map: map,
+                clickable: true,
+                content: 'Map Marker',
+                markerOffset: new google.maps.Point(0,4),
+                title: "latitud:"+lat+ "  longitud:"+lon
+
+              });
+              marker.setMap(map);
+
+            };    
+            createMarker(lat,lon);
+            contenedor.html(html);    
+            
+          });
+            
+        } 
+      });
+    
+
+    });
+  }
+  leeJson();
+};
